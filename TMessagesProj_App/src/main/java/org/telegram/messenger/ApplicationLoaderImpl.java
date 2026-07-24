0f5aad52173d5639475cf749b3bde36ec911b545
Test <test@test.com>
2026-07-24 23:45:54 +0700

    Fix: show update-download progress banner instead of downloading silently
    ApplicationLoaderImpl never overrode takeUpdateLayout(), so MainTabsActivity's
    bottom-of-drawer progress banner (which already listens for
    NotificationCenter.appUpdateLoading/appUpdateAvailable) was never created --
    GithubUpdaterController.downloadUpdate() posted progress correctly the whole
    time, there was just nothing to render it. Port the reference IUpdateLayout
    implementation from TMessagesProj_AppHockeyApp unchanged and wire it up.

--- TMessagesProj_App/src/main/java/org/telegram/messenger/ApplicationLoaderImpl.java
+++ TMessagesProj_App/src/main/java/org/telegram/messenger/ApplicationLoaderImpl.java
@@ -1,10 +1,14 @@
 package org.telegram.messenger;
 
+import android.app.Activity;
 import android.content.Context;
 import android.text.TextUtils;
+import android.view.ViewGroup;
 
 import org.telegram.messenger.regular.BuildConfig;
+import org.telegram.ui.IUpdateLayout;
 import org.telegram.ui.Components.UpdateAppAlertDialog;
+import org.telegram.ui.Components.UpdateLayout;
 
 import java.io.File;
 
@@ -90,5 +94,11 @@ public class ApplicationLoaderImpl extends ApplicationLoader {
             FileLog.e(e);
         }
         return true;
+    }
+
+    @Override
+    public IUpdateLayout takeUpdateLayout(Activity activity, ViewGroup sideMenuContainer) {
+        if (!isCustomUpdate()) return null;
+        return new UpdateLayout(activity, sideMenuContainer);
     }
 }
